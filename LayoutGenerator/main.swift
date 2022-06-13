ecc12e5f3a2e023170d53008590dc9df624e04f4
Ralph Lipe <ralph@lipe.ws>
2022-06-12 22:43:02 -0700

    Improved pruning - but still WAY too many combinations generated

--- LayoutGenerator/main.swift
+++ LayoutGenerator/main.swift
@@ -14,34 +14,43 @@ for id in layoutIds {
     let layout = SuitLayout(suitLayoutId: id)
     print(layout.description)
 }
+*/
 print("Total layouts = \(layoutIds.count)")
- */
-/*
+
+
 let originalCount = layoutIds.count
 print("NOW PRUNING")
 for id in layoutIds {
     let layout = SuitLayout(suitLayoutId: id)
-    print(layout.description, terminator: "")
+    print("\(layout)", terminator: "")
     let analysis = CardCombinationAnalyzer.analyze(suitHolding: SuitHolding(suitLayout: layout))
     let bestLeads = analysis.bestLeads()
-    if bestLeads.first!.combinationsFor(desiredTricks: analysis.maxTricksAllLayouts) == analysis.totalCombinations {
+    if analysis.maxTricksAllLayouts == analysis.worstCaseTricks {
+        print(" - No better than worst case no matter what...  Removed")
+        layoutIds.remove(id)
+    } else if bestLeads.first!.combinationsFor(desiredTricks: analysis.maxTricksAllLayouts) == analysis.totalCombinations {
         print(" - REMOVED - always makes \(analysis.maxTricksAllLayouts)")
         layoutIds.remove(id)
     } else if bestLeads.count == analysis.leads.count {
         print(" - TRIVIAL - all leads make the same tricks!")
+        layoutIds.remove(id)
     } else {
         let maxTricks = bestLeads.first!.maxTricksAnyLayout
         let percent = bestLeads.first!.percentageFor(desiredTricks: maxTricks)
-        print(" - Makes \(maxTricks) \(percent)% of the time")
-        
+        if percent < 1.0 && maxTricks == analysis.worstCaseTricks + 1 {
+            print(" - Some random drop of \(percent)% gets one trick!  Removed")
+            layoutIds.remove(id)
+        } else {
+            print(" - Makes \(maxTricks) \(percent)% of the time")
+        }
     }
 }
 print("After pruning, \(layoutIds.count) reamain of \(originalCount)")
- */
+
 var encoder = JSONEncoder()
 let data = try! encoder.encode(layoutIds)
 let s = String(data: data, encoding: .utf8)
-print(s)
+print(s!)
 var recovered: [Int] = []
 let decoder = JSONDecoder()
 let decodedIDs = try! decoder.decode(Array<Int>.self, from: data)
